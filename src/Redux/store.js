c72d6847addf9fa1567de3e55eee06529dd71bb9
Bipin <biping579@gmail.com>
2023-01-19 19:53:50 +0530

    fp06_459-day3

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore, combineReducers, applyMiddleware } from "redux";
 import thunk from "redux-thunk"
+import { reducer as AppReducer } from "./App/reducer";
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({AppReducer});
 
 export const store = legacy_createStore(rootReducer, applyMiddleware(thunk));
